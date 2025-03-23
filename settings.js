1ba41aae3855a9d4e76fe2319d2cdb0611e0d296
Clemzyjunior9 <clemzyjunior9@gmail.com>
2025-03-22 17:02:43 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '4KT',
+  author: '4KT',
+  botName: "4KT",
+  botOwner: '4KT', // Your name
+  ownerNumber: '233596888329', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
